4e53ad3f7a5df8ad055a4e2c4b479137fddeb742
pedrovmartinsc <pedrovmartinscamara@gmail.com>
2025-07-29 11:24:21 -0300

    aaa

---
+++ README.md
@@ -0,0 +1,14 @@
+~~~ javascript
+
+//console.log("Hello, World!");
+
+//console.log("I like pizza");
+
+//window.alert("This is an aleart")
+//window.alert("I like pizza!");
+
+document.getElementById("myH1").textContent= "Hello";
+document.getElementById("myP").textContent= "I like pizza";
+
+//This is a comment
+~~~ 
--- script2.js
+++ script2.js
@@ -1,4 +1,11 @@
+/* variable = A container that stores a value
+              Behaves as if it were the value it contains
+1. declaration   let x;
+2. assigment x = 100;
 
-if(5>2){
-    console.log("Executou")
-}
+You can only declare a variable one time, you can't declare more than one variable of same name
+
+*/
+let x;
+let y;
+console.log("Hello")
